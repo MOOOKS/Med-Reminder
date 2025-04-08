1ffb33f06c9461ab9f0345326eabaa679d3faf40
Mukund <87586097+MOOOKS@users.noreply.github.com>
2025-04-07 20:40:06 -0700

    Update AppDelegate.swift
    properly closed brackets

--- pill_sorter_app/pill_sorter_app/ios/Runner/AppDelegate.swift
+++ pill_sorter_app/pill_sorter_app/ios/Runner/AppDelegate.swift
@@ -1,5 +1,4 @@
 import UIKit
-
 import Flutter
 import flutter_local_notifications
 
@@ -9,14 +8,16 @@ import flutter_local_notifications
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    
     FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
-    GeneratedPluginRegistrant.register(with: registry)
-
-    GeneratedPluginRegistrant.register(with: self)
-
-    if #available(iOS 10.0, *){
-       UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+      GeneratedPluginRegistrant.register(with: registry)
     }
+
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    }
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
+
