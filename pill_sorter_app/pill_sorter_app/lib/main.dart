eea21a60b51204970bd142187f6bb15ca9f66264
MOOOKS <mathurmukund3@gmail.com>
2025-06-16 09:41:10 -0700

    removed all unessesary files

--- pill_sorter_app/pill_sorter_app/lib/main.dart
+++ pill_sorter_app/pill_sorter_app/lib/main.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
-import 'backend/firebase/firebase_config.dart';
 import 'flutter_flow/flutter_flow_util.dart';
 
 void main() async {
